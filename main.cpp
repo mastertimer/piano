6de1117bc7a2765be2e03727e967719761aa1155
Ivan <mastertimer@mail.ru>
2021-12-11 22:43:10 +0300

    нарисовал нотную линейку

--- main.cpp
+++ main.cpp
@@ -13,18 +13,36 @@ _bitmap paper;
 
 void draw_the_staff()
 {
-	constexpr i64 x_offset = 10;
-	constexpr i64 delta_line = 3;
-	constexpr i64 start_y = delta_line * 3;
-	constexpr i64 staff_size = delta_line * 10;
-	const i64 staff_count = paper.size.y / staff_size;
+	constexpr i64 delta_line = 6;
+
+	constexpr i64 delta5 = 13 * delta_line;
+	constexpr i64 staff_x_size = 125 * delta_line;
+	constexpr i64 x_offset = 9 * delta_line;
+	constexpr i64 staff_y_size = 28 * delta_line;
+
+	constexpr i64 start_y = (staff_y_size - 8 * delta_line) / 4;
+	i64 staff_x_count = std::max(paper.size.x / staff_x_size, 1LL);
+	i64 staff_y_count = std::max(paper.size.y / staff_y_size, 1LL);
+	i64 x_start = (paper.size.x - staff_x_count * staff_x_size) / 2;
+	i64 y_start = (paper.size.y - staff_y_count * staff_y_size) / 2;
 	paper.clear(cc0);
-	for (auto i = 0; i < staff_count; i++)
+	for (auto p = 0; p < staff_x_count; p++)
 	{
-		for (auto j = 0; j < 5; j++)
+		auto x1 = x_start + p * staff_x_size + x_offset;
+		auto x2 = x_start + (p + 1) * staff_x_size - x_offset;
+		for (auto i = 0; i < staff_y_count; i++)
 		{
-			auto y = i * staff_size + start_y + j * delta_line;
-			paper.line({ x_offset, y }, { paper.size.x - x_offset, y }, cc1);
+			for (auto j = 0; j < 5; j++)
+			{
+				auto y = y_start + i * staff_y_size + start_y + j * delta_line;
+				paper.line({ x1, y }, { x2, y }, cc1);
+				paper.line({ x1, y + delta5 }, { x2, y + delta5 }, cc1);
+			}
+			auto y = y_start + i * staff_y_size + start_y;
+			paper.line({ x1, y }, { x1, y + 4 * delta_line + delta5 }, cc1);
+			paper.line({ x2, y }, { x2, y + 4 * delta_line + delta5 }, cc1);
+			paper.text({ x1 + delta_line / 2, y - 3 * delta_line }, L"𝄞", 9 * delta_line, cc1);
+			paper.text({ x1 + delta_line / 2, y - 2 * delta_line + delta5 }, L"𝄢", 7 * delta_line, cc1);
 		}
 	}
 }
@@ -39,6 +57,7 @@ void draw(_isize r)
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	paper.set_font(L"Segoe UI Symbol", false);
 	static bool run_timer = true;
 	static bool tracking_mouse = false;
 	switch (message)
@@ -65,9 +84,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-	wchar_t buffer[MAX_PATH];
-	GetModuleFileName(hInstance, buffer, MAX_PATH);
-
 	static TCHAR szWindowClass[] = L"win64app";
 	WNDCLASSEX wcex;
 	wcex.cbSize = sizeof(WNDCLASSEX);
@@ -83,7 +99,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = 0;
 	if (!RegisterClassEx(&wcex)) return 2;
-	HWND hWnd = CreateWindow(szWindowClass, L"mutator", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1600, 800,
+	HWND hWnd = CreateWindow(szWindowClass, L"piano", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1600, 800,
 		NULL, NULL, hInstance, NULL);
 	if (!hWnd) return 3;
 	ShowWindow(hWnd, nCmdShow);
