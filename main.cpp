5e59bbb78a4ad147f3d60218e97b588d114849ac
Ivan <mastertimer@mail.ru>
2021-12-11 20:07:42 +0300

    старт проекта

---
+++ main.cpp
@@ -0,0 +1,77 @@
+﻿#define NOMINMAX
+#include <windows.h>
+#include "graphics.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+_bitmap master_bm;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void draw(_isize r)
+{
+	if (!master_bm.resize(r)) return;
+	master_bm.clear(0xff000000);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	static bool run_timer = true;
+	static bool tracking_mouse = false;
+	switch (message)
+	{
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		BeginPaint(hWnd, &ps);
+		HDC hdc = GetDC(hWnd);
+		RECT rect;
+		GetClientRect(hWnd, &rect);
+		draw({ rect.right, rect.bottom });
+		BitBlt(hdc, 0, 0, rect.right, rect.bottom, master_bm.hdc, 0, 0, SRCCOPY);
+		ReleaseDC(hWnd, hdc);
+		EndPaint(hWnd, &ps);
+		return 0;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	wchar_t buffer[MAX_PATH];
+	GetModuleFileName(hInstance, buffer, MAX_PATH);
+
+	static TCHAR szWindowClass[] = L"win64app";
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(101)); // 101 из resourse.h
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = 0;
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = 0;
+	if (!RegisterClassEx(&wcex)) return 2;
+	HWND hWnd = CreateWindow(szWindowClass, L"mutator", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1600, 800,
+		NULL, NULL, hInstance, NULL);
+	if (!hWnd) return 3;
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
